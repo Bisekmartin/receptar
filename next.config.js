972e0eff32f280290e927f16c7ceffa8d731164c
Martin Bísek <bisek.martin@gmail.com>
2026-04-21 23:25:38 +0200

    feat: zobrazeni verze v navigaci, npm run push skript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['tesseract.js', 'pdf-parse'],
+  env: {
+    NEXT_PUBLIC_APP_VERSION: process.env.npm_package_version,
+  },
 }
 
 module.exports = nextConfig
